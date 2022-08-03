b7dd4b5d286ec1cb17b96e8368c1bde184004d6c
Ankit Patel <76809891+ankittops301@users.noreply.github.com>
2022-08-03 10:58:42 +0530

    session var

--- Statemanagement/Statemanagement/Controllers/DefaultController.cs
+++ Statemanagement/Statemanagement/Controllers/DefaultController.cs
@@ -18,7 +18,7 @@ namespace Statemanagement.Controllers
         {
             string username = fc["txtuname"];
             string password = fc["txtpass"];
-            if (username=="shankar" && password=="s123")
+            if (username == "shankar" && password == "s123")
             {
                 HttpCookie couname = new HttpCookie("uname");
                 couname.Value = username;
@@ -33,9 +33,54 @@ namespace Statemanagement.Controllers
             return View();
         }
 
-        public ActionResult Qstring(int id,string name)
+        public ActionResult Qstring(int id, string name)
         {
             ViewBag.msg = $"ID: {id}, Name: {name}";
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection fc)
+        {
+            string username = fc["txtuname"];
+            string password = fc["txtpass"];
+            if (username == "shankar" && password == "s123")
+            {
+                Session["uname"] = username;
+                Session["pass"] = password;
+                Session.Timeout = 30;
+                return RedirectToAction("HomePage");
+            }
+            else
+            {
+                ViewBag.loginerror = "Invalid Username or password.!";
+            }
+            return View();
+        }
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult svar()
+        {
+            ViewBag.message = "message from ViewBag";
+            ViewData["msg"] = "displaying ViewData";
+            TempData["data"] = "displaying TempData";
+            return View();
+        }
+
+        public ActionResult second()
+        {
             return View();
         }
     }
