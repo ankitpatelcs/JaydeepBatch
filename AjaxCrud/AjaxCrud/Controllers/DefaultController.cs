8cc4161bc577c69a98088280333932412a85110e
Ankit Patel <76809891+ankittops301@users.noreply.github.com>
2022-08-15 10:45:16 +0530

    ajax

---
+++ AjaxCrud/AjaxCrud/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxCrud.EDM;
+
+namespace AjaxCrud.Controllers
+{
+    public class DefaultController : Controller
+    {
+        CompanyEntities dc = new CompanyEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEmployees()
+        {
+            dc.Configuration.ProxyCreationEnabled = false;
+            return Json(dc.tblemployees.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
