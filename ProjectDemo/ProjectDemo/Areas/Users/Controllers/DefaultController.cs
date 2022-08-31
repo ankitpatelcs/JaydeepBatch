92706bba1d0d2bb5b362c377bc2d724cc68a2439
Ankit Patel <76809891+ankittops301@users.noreply.github.com>
2022-08-31 10:44:48 +0530

    addtocart

--- ProjectDemo/ProjectDemo/Areas/Users/Controllers/DefaultController.cs
+++ ProjectDemo/ProjectDemo/Areas/Users/Controllers/DefaultController.cs
@@ -54,5 +54,23 @@ namespace ProjectDemo.Areas.Users.Controllers
         {
             return View(dc.tblproducts.ToList());
         }
+        public ActionResult Single(int id)
+        {
+            return View(dc.tblproducts.Find(id));
+        }
+
+        public string AddToCart(int id)
+        {
+            int userid = Convert.ToInt32(Session["UserId"].ToString());
+            tblcart obj = new tblcart();
+            obj.product_id = id;
+            obj.qty = 1;
+            obj.user_id = userid;
+
+            dc.tblcarts.Add(obj);
+            dc.SaveChanges();
+
+            return "Product added to cart.";
+        }
     }
 }
