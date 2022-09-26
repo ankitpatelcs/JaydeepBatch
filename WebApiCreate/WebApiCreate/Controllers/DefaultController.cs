9df5d505d96b02d815d38aa7aee21142869d8ded
Ankit <ankitpatel.tops@gmail.com>
2022-09-26 14:40:46 +0530

    apicall

---
+++ WebApiCreate/WebApiCreate/Controllers/DefaultController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApiCreate.EDM;
+
+namespace WebApiCreate.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public async Task<ActionResult> Index()
+        {
+            List<tblemployee> li =null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44335/");
+                var response = await client.GetAsync("api/emp");
+                if (response.IsSuccessStatusCode)
+                {
+                    li = await response.Content.ReadAsAsync<List<tblemployee>>();
+                }
+            }
+            return View(li);
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            tblemployee li = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44335/");
+                var response = await client.GetAsync($"api/emp/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    li = await response.Content.ReadAsAsync<tblemployee>();
+                }
+            }
+            return View(li);
+        }
+    }
+}
