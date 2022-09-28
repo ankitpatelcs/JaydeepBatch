0baf96ee0a4d95caa01d6d454a909913fad3ed7e
Ankit <ankitpatel.tops@gmail.com>
2022-09-28 08:27:16 +0530

    api calling

--- WebApiCreate/WebApiCreate/Controllers/DefaultController.cs
+++ WebApiCreate/WebApiCreate/Controllers/DefaultController.cs
@@ -15,7 +15,7 @@ namespace WebApiCreate.Controllers
         // GET: Default
         public async Task<ActionResult> Index()
         {
-            List<tblemployee> li =null;
+            List<tblemployee> li = null;
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("https://localhost:44335/");
@@ -41,6 +41,85 @@ namespace WebApiCreate.Controllers
                 }
             }
             return View(li);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(tblemployee obj)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44335/");
+                var response = await client.PostAsJsonAsync<tblemployee>($"api/emp", obj);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Error.!");
+            return View();
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            tblemployee li = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44335/");
+                var response = await client.GetAsync($"api/emp/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    li = await response.Content.ReadAsAsync<tblemployee>();
+                }
+            }
+            return View(li);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Edit(tblemployee obj)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44335/");
+                var response = await client.PutAsJsonAsync<tblemployee>($"api/emp/{obj.emp_id}", obj);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Error.!");
+            return View();
+        }
+        public async Task<ActionResult> Delete(int id)
+        {
+            tblemployee li = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44335/");
+                var response = await client.GetAsync($"api/emp/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    li = await response.Content.ReadAsAsync<tblemployee>();
+                }
+            }
+            return View(li);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<ActionResult> DeleteRec(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44335/");
+                var response = await client.DeleteAsync($"api/emp/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Error.!");
+            return View();
         }
     }
 }
